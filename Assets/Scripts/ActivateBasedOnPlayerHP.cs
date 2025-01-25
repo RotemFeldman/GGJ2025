10e31a9da32b0abb3fd4ebbe55289843b72fb12f
RotemFeldman <128305863+RotemFeldman@users.noreply.github.com>
2025-01-25 12:49:07 +0300

    script

---
+++ Assets/Scripts/ActivateBasedOnPlayerHP.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class ActivateBasedOnPlayerHP : MonoBehaviour
+    {
+        [Range(0f,1f)]
+        public float Threshold;
+        public bool ActiveWhenAbovethreshold;
+
+        private void Start()
+        {
+            PlayerHealth.Instance.HealthPercent += Foo;
+        }
+
+        private void Foo(float obj)
+        {
+            if (ActiveWhenAbovethreshold)
+            {
+                if (obj > Threshold)
+                {
+                    gameObject.SetActive(true);
+                }
+                else
+                {
+                    gameObject.SetActive(false);
+                }
+            }
+            else
+            {
+                if (obj > Threshold)
+                {
+                    gameObject.SetActive(false);
+                }
+                else
+                {
+                    gameObject.SetActive(true);
+                }
+            }
+        }
+    }
+}
